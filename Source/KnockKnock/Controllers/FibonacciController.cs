d9d05999ad7728712fa3d548a831c09519c3a247
Steve Shearn <steve@syntronian.com>
2017-04-18 16:03:53 +1000

    ok, i'll do it myself instead of swiping for a nuget

--- Source/KnockKnock/Controllers/FibonacciController.cs
+++ Source/KnockKnock/Controllers/FibonacciController.cs
@@ -12,7 +12,22 @@ namespace KnockKnock.Controllers
     {
         public long Get(int n)
         {
-            return Fibonaci.Nth((long) n);            
-        }
+            long f = 0;
+            long s = 1;
+            long p = f + s;
+
+            if (n == 0) return f;
+            if (n == 1) return s;
+            if (n == 2) return p;
+
+            for(var i = 0; i < n - 2; i++)
+            {
+                f = s;
+                s = p;
+                p = f + s;
+            }
+
+            return p;
+        }        
     }
 }
